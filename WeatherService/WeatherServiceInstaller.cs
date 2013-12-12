205643159c7951abaf607ca821fa2205defe4622
v.pestov <pestovvladimir@gmail.com>
2013-12-12 18:08:11 +0600

    Weather services used together

---
+++ WeatherService/WeatherServiceInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using WeatherService.Services;
+
+namespace WeatherService
+{
+    public class WeatherServiceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                    .Where(Component.IsInSameNamespaceAs<IWeatherService>())
+                    .WithService.DefaultInterfaces()
+                    .LifestyleSingleton());
+        }
+    }
+}
--- WeatherServiceClient/Program.cs
+++ WeatherServiceClient/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using AutoMapper;
+using Castle.Windsor;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
@@ -17,19 +18,34 @@ namespace WeatherServiceClient
     {
         static void Main(string[] args)
         {
-            var service = new OpenWeatherService();
-
-            var info = service.GetWeatherInfo("Chelyabinsk");
+            var container = new WindsorContainer();
+            container.Install(new WeatherServiceInstaller());
 
             Mapper
             .CreateMap<Dictionary<string, string>, WeatherInfoModel>()
             .ConvertUsing(x =>
-                {
-                    var serializer = new JavaScriptSerializer();
-                    return serializer.Deserialize<WeatherInfoModel>(serializer.Serialize(x));
-                });
+            {
+                var serializer = new JavaScriptSerializer();
+                return serializer.Deserialize<WeatherInfoModel>(serializer.Serialize(x));
+            });
 
-            var model = Mapper.Map<Dictionary<string, string>, WeatherInfoModel>(info);
+            var services = container.ResolveAll<IWeatherService>();
+
+            var allWeatherProperties = new Dictionary<string, string>();
+            foreach (var weatherService in services)
+            {
+                var currentServiceWeatherProperties = weatherService.GetWeatherInfo("Chelyabinsk");
+
+                foreach (var weatherProperties in currentServiceWeatherProperties)
+                {
+                    if (!allWeatherProperties.ContainsKey(weatherProperties.Key))
+                    {
+                        allWeatherProperties[weatherProperties.Key] = weatherProperties.Value;
+                    }
+                }
+            }
+            
+            var model = Mapper.Map<Dictionary<string, string>, WeatherInfoModel>(allWeatherProperties);
 
             Console.ReadKey();
         }
