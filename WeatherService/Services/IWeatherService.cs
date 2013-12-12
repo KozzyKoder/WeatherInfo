ffd75f778c47e1b21788e76c912710d90bbfb75f
v.pestov <pestovvladimir@gmail.com>
2013-12-12 15:39:36 +0600

    Added Interface

---
+++ WeatherService/Services/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherService.Services
+{
+    public interface IWeatherService
+    {
+        Dictionary<string, string> GetWeatherInfo(string cityName);
+    }
+}
